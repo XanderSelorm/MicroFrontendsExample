86bdcb63304961460cfe90461fd9b2a7b107e30d
Xander Selorm <alexandercubby@gmail.com>
2022-01-27 23:42:56 +0000

    WIP

---
+++ MicroFrontendsExample.Shell/webpack.config.js.js
@@ -0,0 +1,21 @@
+﻿module.exports = [
+    {
+        name: "get-react",
+        entry: () => fs.readdirSync("../ReactHello/wwwroot/dist/").filter(f => f.endsWith(".js")).map(f => `../ReactMicroFrontend/wwwroot/dist/react/${f}`),
+        devtool: "source-map",
+        mode: "development",
+        output: {
+            filename: "app.js",
+            path: path.resolve(__dirname, "./wwwroot/dist/react/")
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    enforce: "pre",
+                    use: ["source-map-loader"]
+                }
+            ]
+        }
+    }
+]
