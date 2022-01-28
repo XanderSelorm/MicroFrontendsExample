87d16376a735c25985b5fa07df332c57199530f7
Xander Selorm <alexandercubby@gmail.com>
2022-01-28 19:58:57 +0000

    Added code behind for App.razor in MF

---
+++ ReactMicroFrontend/App.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactMicroFrontend
+{
+    public partial class App
+    {
+        [Inject]
+        IJSRuntime Jsr { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await Jsr.InvokeVoidAsync("renderApp");
+            await base.OnAfterRenderAsync(firstRender);
+
+        }
+    }
+}
